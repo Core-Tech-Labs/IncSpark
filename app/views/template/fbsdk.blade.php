82e9baa99ed923e73beed8265e82d00f64769615
Rudy Jessop <rudyjessop@gmail.com>
2014-10-23 02:20:31 -0400

    general commit

---
+++ app/views/template/fbsdk.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * Copyright 2014 Core Tech Labs, Inc
+ * Author: Rudy Jessop
+ * Author URL: http://rudyjessop.com
+ * Core Tech Labs, Inc is the sole owner of this software. No distribution no rediting without written concent.
+ */
+
+/*
+
+ * This file holds the Facebook login SDK. All edits should be done in here in relations to Facebook login.
+ *  */
