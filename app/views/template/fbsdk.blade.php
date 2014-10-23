142ac97537166d655cbdd46277d20461b66a77fc
Rudy Jessop <rudyjessop@gmail.com>
2014-10-23 13:10:59 -0400

    Gerneral Commit

--- app/views/template/fbsdk.blade.php
+++ app/views/template/fbsdk.blade.php
@@ -1,13 +1,13 @@
-<?php
+@section("fbsdk")
+   <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=757777707621496&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
 
-/* 
- * Copyright 2014 Core Tech Labs, Inc
- * Author: Rudy Jessop
- * Author URL: http://rudyjessop.com
- * Core Tech Labs, Inc is the sole owner of this software. No distribution no rediting without written concent.
- */
+@show
 
-/*
-
- * This file holds the Facebook login SDK. All edits should be done in here in relations to Facebook login.
- *  */
--- app/views/template/header.blade.php
+++ app/views/template/header.blade.php
@@ -19,8 +19,43 @@
       
     </head>
     <header>
-        <div class="logo">IncSpark</div>
-        <nav class="menu-nav"></nav>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <!-- Mobile nav-->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                  </button>
+         <a class="navbar-brand" href="#">IncSpark</a>
+         
+                </div>
+                <!-- Mobile nav-->
+                
+                <!-- Desktop -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li ><a href="#">Nav Item 1</a></li>
+                        <li ><a href="#">Nav Item 2</a></li>
+                        <li ><a href="#">Nav Item 3</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login | Sign Up
+                                <span class="caret"></span>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Sub-menu1</a></li>
+                                    <li class="divider"></li>
+                                    <li id="comestic-login"><div class="fb-login-button" data-max-rows="1" data-size="large" data-show-faces="false" data-auto-logout-link="true"></div></li>
+                                </ul>
+                            </a></li>
+                    </ul>
+                </div>
+                <!-- Desktop -->
+            </div>
+        </nav>
     </header>
+    
+ @include("template/fbsdk")
     <body>
 @show
