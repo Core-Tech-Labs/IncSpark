ed4391ec7eda018e76124665fbab97e57ccd38f5
Rudy Jessop <rudyjessop@gmail.com>
2014-11-13 21:59:48 -0500

    UI commit

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,7 +1,39 @@
 @include("template/header")
 
-<div class="container" id="comestic-container-body">
-      <h1>This is IncSpark</h1>
+<div class="width-container" id="comestic-container-body">
+      <div id="comestic-incspark-img">
+          
+      </div>
+    <div class="width-container" id="comestic-featuredEvents-vo">
+        <div class="container">
+        <h2>Featured Business Events</h2>
+        <br/>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <div id="comestic-thumbnail-vo"></div>
+                          <div class="caption">
+                            <h3>Event Name</h3>
+                            <span>Event Time</span>
+                            <span>Event Location</span>
+                            
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="width-container" id="comestic-featuredEvents-ve">
+        <div class="container">
+        <h2>Popular Business Events</h2>
+        </div>
+    </div>
+    <div class="width-container" id="comestic-featuredEvents">
+        <div class="container">
+        <h2>Filtered Categories</h2>
+        
+        </div>
+    </div>
 </div>
     
       
