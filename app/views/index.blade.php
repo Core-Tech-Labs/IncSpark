252b7a9bc782d18e09eaa54760ecc159e6344836
Rudy Jessop <rudyjessop@gmail.com>
2014-11-13 23:50:51 -0500

    UI commit

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,13 +10,12 @@
         <br/>
             <div class="row">
                 <div class="col-sm-6 col-md-4">
-                    <div class="thumbnail">
+                    <div class="thumbnail" id="comestic-finetune">
                         <div id="comestic-thumbnail-vo"></div>
-                          <div class="caption">
-                            <h3>Event Name</h3>
-                            <span>Event Time</span>
-                            <span>Event Location</span>
-                            
+                          <div class="caption" >
+                              <h3><a href="#">Event Name</a></h3>
+                            <p>Event Time</p>
+                            <p>Event Location</p>
                           </div>
                     </div>
                 </div>
@@ -26,12 +25,65 @@
     <div class="width-container" id="comestic-featuredEvents-ve">
         <div class="container">
         <h2>Popular Business Events</h2>
+        <br/>
+             <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail" id="comestic-finetune">
+                        <div id="comestic-thumbnail-vo"></div>
+                          <div class="caption" >
+                              <h3><a href="#">Event Name</a></h3>
+                            <p>Event Time</p>
+                            <p>Event Location</p>
+                          </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="comestic-button-center">
+            <button type="button" class="btn btn-default">See More</button>
         </div>
     </div>
     <div class="width-container" id="comestic-featuredEvents">
         <div class="container">
         <h2>Filtered Categories</h2>
+        <br/>
+            <!-- md x4-->
+            <div class="row">
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve"></div>
+                    </a>
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve"></div>
+                    </a>
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve"></div>
+                    </a>
+                </div>
+                <div class="col-xs-6 col-md-3">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve"></div>
+                    </a>
+                </div>
+            </div>
         
+            <!-- xs x2 -->
+            <div class="row">
+                <div class="col-xs-6 col-md-6">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve1"></div>
+                    </a>
+                </div>
+                <div class="col-xs-6 col-md-6">
+                    <a href="#" class="thumbnail">
+                        <div id="comestic-thumbnail-ve1"></div>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
--- app/views/template/header.blade.php
+++ app/views/template/header.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-          <title>The World's First Entrepreneurial Events Portal</title>
+          <title>Inc Spark | The World's First Entrepreneurial Events Portal</title>
           <!-- Template
           
         <link type="text/css" rel="stylesheet" href="/bootstrap/css"/>
