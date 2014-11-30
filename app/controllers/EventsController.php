1572b8d5b59c8530dfaf723718d5f166da33dfc1
Rudy Jessop <rudyjessop@gmail.com>
2014-11-30 09:28:25 -0500

    regular commit

---
+++ app/controllers/EventsController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class EventsController extends BaseController{
+    
+    // Page setup
+    public function eventsPage(){
+        return View::make();
+    }
+    
+    // API For events from other sources.
+    public function eventSets(){
+        
+    }
+}
--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -5,4 +5,10 @@ class UserController extends BaseController{
     public function userPage(){
         return View::make('user.UserIndex');
     }
+    
+    private function fbUserLogin(){
+        
+    }
+    
+    
 }
